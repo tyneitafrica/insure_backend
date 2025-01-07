0e0f0dd9f38ac36deac5da1ee2a53d17d8f8b057
Clement Macharia <clementmacharia62@gmail.com>
2025-01-07 15:52:26 +0300

    initial layout structure

---
+++ insure_app/insure/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.site_header = "INSURE DASHBOARD "
+admin.site.site_title = "INSURE"
+admin.site.index_title = "Welcome to Your your Dashboard"
---
+++ insure_app/insure/management/commands/seed.py
@@ -0,0 +1,1 @@
+# will handle the initial seed data
