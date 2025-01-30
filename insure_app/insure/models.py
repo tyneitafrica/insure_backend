cebb70388b259ec9b86e5e0c8603ddf8cb8ab7b8
Clement Macharia <clementmacharia62@gmail.com>
2025-01-30 16:09:47 +0300

    updated models with str

--- insure_app/insure/models.py
+++ insure_app/insure/models.py
@@ -119,7 +119,7 @@ class MotorInsurance(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.price} - {self.cover_type}"
+        return f"{self.price} - {self.cover_type} - {self.insurance}"
 
 
 # Health Insurance Model
