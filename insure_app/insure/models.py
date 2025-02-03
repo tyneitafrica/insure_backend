ffa76d75aeda6d506cec0443412cbdce1211579d
davidmuchoki <dmmuchoki7@gmail.com>
2025-02-03 16:49:08 +0300

    add company_name field to Insurance model and timestamps to HealthInsurance model

--- insure_app/insure/models.py
+++ insure_app/insure/models.py
@@ -113,6 +113,7 @@ class Insurance(models.Model):
         ("Professional Indemnity", "Professional Indemnity"),
     ]
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE, related_name='insurances')
+    company_name= models.CharField(max_length=200, null=True, blank=True)
     insurance_image = models.ImageField(upload_to='insurance_images/', null=True, blank=True) #will handle profile image of a specific insurance 
     type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     title = models.CharField(max_length=100)
@@ -141,6 +142,8 @@ class HealthInsurance(models.Model):
     low_range= models.IntegerField()
     cover_type = models.CharField(max_length=100)  # e.g., Individual, Family
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at= models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.cover_type} - {self.price}"
