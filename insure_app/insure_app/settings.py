688458ce572e92e1f0b3f68f753654274ad3f338
Clement Macharia <clementmacharia62@gmail.com>
2025-02-18 16:36:39 +0300

    settings

--- insure_app/insure_app/settings.py
+++ insure_app/insure_app/settings.py
@@ -84,27 +84,27 @@ WSGI_APPLICATION = 'insure_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'insure_db.sqlite3',
-#     }
-# }
-
-# postgres database 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': 'require',  # This enforces SSL encryption
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'insure_db.sqlite3',
     }
 }
+
+# postgres database 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#         'OPTIONS': {
+#             'sslmode': 'require',  # This enforces SSL encryption
+#         },
+#     }
+# }
 
 
 # Password validation
